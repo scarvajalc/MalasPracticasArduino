d998d076fb0a1b6a9796c631a1262137d3befb48
David Beltrán <debeltranc@unal.edu.co>
2018-05-25 11:50:30 -0500

    Include wifi functions

--- v2.ino
+++ v2.ino
@@ -11,7 +11,14 @@ const int ultraSonicSenorTrigger1 = 5;
 //Time
 unsigned long time;
 unsigned long waitTime = 3000;
-unsigned long lastTiltOcurrence = -waitTime ; 
+unsigned long lastTiltOcurrence = -waitTime ;
+//wifi
+String ssid ="yourSSID";
+String password="yourPassword";
+SoftwareSerial esp(6, 7);// RX, TX
+String data;
+String server = "yourServer"; // www.example.com
+String uri = "yourURI";// our example is /esppost.php
 
 struct badPractice
 {
@@ -29,8 +36,13 @@ void setup() {
   pinMode(tiltSensor, INPUT);
   pinMode(ultraSonicSenorEcho1, INPUT);
   pinMode(ultraSonicSenorTrigger1, OUTPUT);
+
+  //wifi
+  esp.begin(9600);
+  reset();
+  connectWifi();
   // put your setup code here, to run once:
-  
+
 }
 
 void checkTilt(){
@@ -64,6 +76,60 @@ void printBpReport(){
   }
 }
 
+//WiFi Functions ===================================================================
+
+//reset the esp8266 module
+void reset() {
+  esp.println("AT+RST");
+  delay(1000);
+  if(esp.find("OK") ) Serial.println("Module Reset");
+}
+
+//connect to your wifi network
+void connectWifi() {
+  String cmd = "AT+CWJAP=\"" +ssid+"\",\"" + password + "\"";
+  esp.println(cmd);
+  delay(4000);
+  if(esp.find("OK")) {
+    Serial.println("Connected!");
+  }
+  else {
+    connectWifi();
+    Serial.println("Cannot connect to wifi");
+  }
+}
+
+//send POST request
+void httppost () {
+  esp.println("AT+CIPSTART=\"TCP\",\"" + server + "\",80");//start a TCP connection.
+  if( esp.find("OK"))
+    Serial.println("TCP connection ready");
+  delay(1000);
+  String postRequest = "POST " + uri + " HTTP/1.0\r\n" +
+                       "Host: " + server + "\r\n" +
+                       "Accept: *" + "/" + "*\r\n" +
+                       "Content-Length: " + data.length() + "\r\n" +
+                       "Content-Type: application/x-www-form-urlencoded\r\n" +
+                       "\r\n" + data;
+  String sendCmd = "AT+CIPSEND=";//determine the number of caracters to be sent.
+  esp.print(sendCmd);
+  esp.println(postRequest.length() );
+  delay(500);
+  if(esp.find(">")) {
+    Serial.println("Sending..");
+    esp.print(postRequest);
+    if( esp.find("SEND OK")) {
+      Serial.println("Packet sent");
+      while (esp.available()) {
+        String tmpResp = esp.readString();
+        Serial.println(tmpResp);
+      }
+      // close the connection
+      esp.println("AT+CIPCLOSE");
+    }
+  }
+}
+
 
 void loop() {
   //Serial.print()
@@ -73,5 +139,5 @@ void loop() {
     //printBpReport();
   }
 
-  
+
 }
